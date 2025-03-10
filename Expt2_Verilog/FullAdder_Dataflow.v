1da000f4c625d2e6685567f3d7083a8fecfc6d0e
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-03-10 21:02:04 +0530

    Create FullAdder_Dataflow.v

---
+++ Expt2_Verilog/FullAdder_Dataflow.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 10.03.2025 12:08:25
+// Design Name:
+// Module Name: FullAdder_Dataflow
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FullAdder_Dataflow(sum,carry,a,b,c);
+output sum,carry;
+input a,b,c;
+assign sum=a^b^c;
+assign carry=(a&b)|(b&c)|(c&a);
+endmodule
