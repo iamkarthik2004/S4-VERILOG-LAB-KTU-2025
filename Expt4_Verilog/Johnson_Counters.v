29a18d5021d924e13da1dfe16ceed1c32457de47
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-04-08 09:29:24 +0530

    Update Johnson_Counters.v

--- Expt4_Verilog/Johnson_Counters.v
+++ Expt4_Verilog/Johnson_Counters.v
@@ -1,4 +1,4 @@
-module johnson (input clk, output reg[3:0] = 4'b1000;
+module johnson (input clk, output reg[3:0]q = 4'b1000;
                 always@(posedge clk)
                   begin
                     4'b0000:q=4'b1000;
